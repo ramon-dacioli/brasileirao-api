4cb52c1b00bbfb313f68dc2219b0b611f9b44371
Ramon Dacioli <ramon_muz@hotmail.com>
2022-12-22 09:58:15 -0300

    commit inicial

---
+++ src/test/java/com/devqualisys/brasileiraoapi/BrasileiraoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devqualisys.brasileiraoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasileiraoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
